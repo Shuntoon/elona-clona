e4edfa43338818504eb78e87770c1ac1c0594b24
Shane Huntoon <Shuntoon555@gmail.com>
2025-11-20 19:38:28 -0500

    added hitbox data

--- Data/EnemyData/enemy_data.gd
+++ Data/EnemyData/enemy_data.gd
@@ -8,7 +8,14 @@ class_name EnemyData
 @export var range : float = 30
 @export var damage : int = 1
 @export var sprite : Texture2D
+@export var animated_sprite_frames : SpriteFrames
 @export var attack_speed : float = 1
 @export var gold_reward : int = 5
 @export var gold_reward_variance : int = 2
 @export var terrain_type : Enemy.TERRAIN_TYPE
+
+@export var body_hitbox_size : Vector2 = Vector2(16, 16)
+@export var body_pos : Vector2 = Vector2(0, 0)
+
+@export var head_hitbox_size : Vector2 = Vector2(12, 12)
+@export var head_pos : Vector2 = Vector2(0, -10)
--- Objects/Hurtbox/hurtbox.gd
+++ Objects/Hurtbox/hurtbox.gd
@@ -7,3 +7,6 @@ enum HURTBOX_TYPE {
 }
 
 @export var hurtbox_type : HURTBOX_TYPE
+
+@export var size : Vector2 = Vector2(16, 16)
+@export var pos : Vector2 = Vector2(0, 0)
